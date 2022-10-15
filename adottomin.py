17a7b3e98138655b4e5ae85b5d416a133869a22a
PYROP3 <caiotsan+github@gmail.com>
2022-10-14 21:46:39 -0300

    Fix

--- adottomin.py
+++ adottomin.py
@@ -227,9 +227,9 @@ async def _handle_nsfw_added(before: discord.Member, after: discord.Member):
     if not utils.just_joined(before.id): 
         logger.info(f"{after} added nsfw role and is a previous user")
         return
-    # nsfw_role = [role for role in after.roles if role.id == nsfw_role_id]
+    nsfw_role = [role for role in after.roles if role.id == nsfw_role_id]
     logger.info(f"{after} added nsfw role but is still being verified")
-    await after.remove_roles([nsfw_role_id])
+    await after.remove_roles(nsfw_role, reason="Not verified")
     notif = after.guild.get_channel(channel_ids[0])
     await notif.send(content=f"Straight for the NSFW and didn't even tell me your age, {after.mention}?~")
 
