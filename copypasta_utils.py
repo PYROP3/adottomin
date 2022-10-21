c5e2e161e47747653c89626565abfd021dc54eed
PYROP3 <caiotsan+github@gmail.com>
2022-10-21 16:54:20 -0300

    Improve copypasta tools

--- copypasta_utils.py
+++ copypasta_utils.py
@@ -1,26 +1,37 @@
-import copypasta_data
+try:
+    import copypasta_data
 
-PRON_M = "he"
-PRON_F = "she"
-PRON_N = "they"
-PRON_T = "it"
+    def _and_capitalized(*prons):
+        return prons + tuple(pron.capitalize() for pron in prons)
 
-PRON_OPTS = [PRON_M, PRON_F, PRON_N, PRON_T]
+    PRONOUNS = {# pr, subj, poss, to-be
+        prons[0]: _and_capitalized(*prons) for prons in [
+            ("he", "him", "his", "he's"),
+            ("she", "her", "her", "she's"),
+            ("they", "them", "their", "they're"),
+            ("it", "it", "its", "it's"),
+        ]
+    }
 
-PRONOUNS = {
-    PRON_M: ("he", "him", "his", "he's"),
-    PRON_F: ("she", "her", "her", "she's"),
-    PRON_N: ("they", "them", "their", "they're"),
-    PRON_T: ("it", "it", "its", "it's"),
-}
+    PRON_OPTS = list(PRONOUNS.keys())
 
-AVAILABLE_PASTAS = list(copypasta_data.copypasta_list.keys())
+    AVAILABLE_PASTAS = list(copypasta_data.copypasta_list.keys())
 
-def fill_copypasta(key, target, pronoun):
-    pron, subj_pron, poss_pron, pron_is = PRONOUNS[pronoun]
-    return copypasta_data.copypasta_list[key].format(
-        target=target, 
-        pron=pron, 
-        subj_pron=subj_pron, 
-        poss_pron=poss_pron, 
-        pron_is=pron_is)
+    def fill_copypasta(key, target, pronoun):
+        pron, subj_pron, poss_pron, pron_is, Pron, Subj_pron, Poss_pron, Pron_is = PRONOUNS[pronoun]
+        return copypasta_data.copypasta_list[key].format(
+            target=target, 
+            pron=pron, 
+            subj_pron=subj_pron, 
+            poss_pron=poss_pron, 
+            pron_is=pron_is,
+            Pron=Pron,
+            Subj_pron=Subj_pron,
+            Poss_pron=Poss_pron,
+            Pron_is=Pron_is)
+            
+except:
+    import sys
+    print("Copypasta data not found, ignoring...", file=sys.stderr)
+    PRON_OPTS = []
+    AVAILABLE_PASTAS = []
