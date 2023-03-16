74f2eaf02a20a3f84656d1e5373f6f8480e37f0f
PYROP3 <caiotsan+github@gmail.com>
2023-03-16 19:48:19 -0300

    Improve copypasta generation

--- copypasta_utils.py
+++ copypasta_utils.py
@@ -4,12 +4,12 @@ try:
     def _and_capitalized(*prons):
         return prons + tuple(pron.capitalize() for pron in prons)
 
-    PRONOUNS = {# pr, subj, poss, to-be
+    PRONOUNS = {# pr, subj, poss, to-be verb-to-be
         prons[0]: _and_capitalized(*prons) for prons in [
-            ("he", "him", "his", "he's"),
-            ("she", "her", "her", "she's"),
-            ("they", "them", "their", "they're"),
-            ("it", "it", "its", "it's"),
+            ("he", "him", "his", "he's", "is"),
+            ("she", "her", "her", "she's", "is"),
+            ("they", "them", "their", "they're", "are"),
+            ("it", "it", "its", "it's", "is"),
         ]
     }
 
@@ -18,17 +18,19 @@ try:
     AVAILABLE_PASTAS = list(copypasta_data.copypasta_list.keys())
 
     def fill_copypasta(key, target, pronoun):
-        pron, subj_pron, poss_pron, pron_is, Pron, Subj_pron, Poss_pron, Pron_is = PRONOUNS[pronoun]
+        pron, subj_pron, poss_pron, pron_is, verb_to_be, Pron, Subj_pron, Poss_pron, Pron_is, Verb_to_be = PRONOUNS[pronoun]
         return copypasta_data.copypasta_list[key].format(
             target=target, 
             pron=pron, 
             subj_pron=subj_pron, 
             poss_pron=poss_pron, 
             pron_is=pron_is,
+            verb_to_be=verb_to_be,
             Pron=Pron,
             Subj_pron=Subj_pron,
             Poss_pron=Poss_pron,
-            Pron_is=Pron_is)
+            Pron_is=Pron_is,
+            Verb_to_be=Verb_to_be)
             
 except:
     import sys
