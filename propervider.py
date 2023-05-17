b104a396cc92852a6c5ab8454b08c87fddf69dbd
PYROP3 <caiotsan+github@gmail.com>
2023-05-16 23:39:39 -0300

    Property provider bugfix

--- propervider.py
+++ propervider.py
@@ -7,13 +7,13 @@ def pint(property:str, required:bool=True):
     prop = os.getenv(property)
     if required and not prop:
         raise RuntimeError(f"Missing required env variable {property}")
-    return int(prop)
+    return prop and int(prop)
 
 def plist(property:str, required:bool=True):
     aux = os.getenv(property)
     if required and not aux:
         raise RuntimeError(f"Missing required env variable {property}")
-    return [] if not aux else [int(elem) for elem in aux.split('.')]
+    return [] if not aux else [elem and int(elem) for elem in aux.split('.')]
 
 def pstr(property:str, required:bool=True):
     prop = os.getenv(property)
